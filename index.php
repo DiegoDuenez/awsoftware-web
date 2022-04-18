edd1510f425b5d897d414c1f2e9b3cd78deeb1d5
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-04-18 18:22:50 -0500

    init

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> A W Software | Desarrollo de Aplicaciones Moviles, Software, Realidad Virtual, páginas web, tiendas en línea y hosting en Torreón</title>
+    <!--===== FONTAWESOME ICONS =====-->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />-->
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.0/css/all.css">
+    <!--====== AOS CSS ======-->
+    <link rel="stylesheet" href="libs/aos/dist/aos.css">
+    <!--====== MAIN CSS ======-->
+    <link rel="stylesheet" href="css/index.css">
+    <!--====== GOOGLE RECAPTCHA ======-->
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+</head>
+<body>
+
+
+    <?php include('templates/components/buttons.html') ?>
+
+    <?php include('templates/components/nav.html') ?>
+
+    <?php include('templates/layouts/apps.html') ?>
+
+     <!--=== JQUERY ===-->
+     <script src="libs/jquery/jquery,min.js"></script>
+    <!--=== AOS JS ===-->
+    <script src="libs/aos/dist/aos.js"></script>
+  
+
+
+    
+</body>
+</html>
