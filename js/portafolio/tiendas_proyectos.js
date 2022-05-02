28ce88445b0ef7a917611f26106d46e71ea0d458
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-05-02 11:31:43 -0500

    upd: Se actualizo portafolio de tiendas

--- js/portafolio/tiendas_proyectos.js
+++ js/portafolio/tiendas_proyectos.js
@@ -7,73 +7,106 @@
 
 
 
-var cliente = 'ikigai'
+
+
+var clienteViewTiendas = 1;
+
 var img = 'assets/img/proyectos/tiendas/IKIGAI.png'
 var descripcion = ''
 var cliente = `<span class="proyectos__text--azul">01 - CLIENTE:</span> Ikigai </p>`
 
 
-$('#tiendas_proyectos_body').on('click','.proyectos__circles *', function() {
+$('#siguienteClienteTiendas').click(function(){
 
-
-    cliente = $(this).attr('id')
-    $(this).data('clicked', true);
-
-    if($(this).data('clicked')){
-
-        $(".proyectos__circles *").each(function() {   
-
-            var element = $(this);
-            
-            if(!element.is("#"+cliente)){
-                if(element.hasClass('proyectos__circle--active')){
-                    element.removeClass('proyectos__circle--active')
-                }
-            }
-            
-        })
-        $(this).addClass('proyectos__circle--active')
-    
-    }
-
-
-    if(cliente == 'ikigai'){
-        
-        img  = 'assets/img/proyectos/tiendas/IKIGAI.png'
+    if(clienteViewTiendas == 1){
+        img  = 'assets/img/proyectos/tiendas/MANAGERMAC.png'
         descripcion = ''
-        cliente = `<span class="proyectos__text--azul">01 - CLIENTE:</span> Ikigai </p>`
+        cliente = `<span class="proyectos__text--azul">02 - CLIENTE:</span> Managermac </p>`
 
+        clienteViewTiendas = 2
+
+        $('#tiendas_proyectos_descripcion').fadeOut(500, function() {
+        }).fadeIn(500);
+    
+        $('#tiendas_proyectos_cliente').fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_descripcion').empty().show()
+            $('#tiendas_proyectos_descripcion').prepend(`<p class="proyectos__text" id="tiendas_proyectos_cliente">${cliente}</p> ` + descripcion)
+        }).fadeTo(500,1);
+    
+        $("#tiendas_proyectos_img").fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_img').attr("src", img);
+        }).fadeTo(500,1);
+        return false;
     }
-    else if(cliente == 'managermac'){
+    else if(clienteViewTiendas == 2){
+        img  = 'assets/img/proyectos/tiendas/ROMANA.png'
+        descripcion = ''
+        cliente = `<span class="proyectos__text--azul">03 - CLIENTE:</span> Romana </p>`
+
+        clienteViewTiendas = 3
+
+        $('#tiendas_proyectos_descripcion').fadeOut(500, function() {
+        }).fadeIn(500);
+    
+        $('#tiendas_proyectos_cliente').fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_descripcion').empty().show()
+            $('#tiendas_proyectos_descripcion').prepend(`<p class="proyectos__text" id="tiendas_proyectos_cliente">${cliente}</p> ` + descripcion)
+        }).fadeTo(500,1);
+    
+        $("#tiendas_proyectos_img").fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_img').attr("src", img);
+        }).fadeTo(500,1);
+        return false;
+    }
+
+})
+
+
+$('#anteriorClienteTiendas').click(function(){
+
+    if(clienteViewTiendas == 3){
 
         img  = 'assets/img/proyectos/tiendas/MANAGERMAC.png'
         descripcion = ''
         cliente = `<span class="proyectos__text--azul">02 - CLIENTE:</span> Managermac </p>`
 
-    }
-    else if(cliente == 'romana'){
+        clienteViewTiendas = 2
 
-        img  = 'assets/img/proyectos/tiendas/ROMANA.png'
+        $('#tiendas_proyectos_descripcion').fadeOut(500, function() {
+        }).fadeIn(500);
+    
+        $('#tiendas_proyectos_cliente').fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_descripcion').empty().show()
+            $('#tiendas_proyectos_descripcion').prepend(`<p class="proyectos__text" id="tiendas_proyectos_cliente">${cliente}</p> ` + descripcion)
+        }).fadeTo(500,1);
+    
+        $("#tiendas_proyectos_img").fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_img').attr("src", img);
+        }).fadeTo(500,1);
+        return false;
+
+    }
+    else if(clienteViewTiendas == 2){
+
+        img  = 'assets/img/proyectos/tiendas/IKIGAI.png'
         descripcion = ''
-        cliente = `<span class="proyectos__text--azul">03 - CLIENTE:</span> Romana </p>`
-        
+        cliente = `<span class="proyectos__text--azul">01 - CLIENTE:</span> Ikigai </p>`
+
+        clienteViewTiendas = 1
+
+        $('#tiendas_proyectos_descripcion').fadeOut(500, function() {
+        }).fadeIn(500);
+    
+        $('#tiendas_proyectos_cliente').fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_descripcion').empty().show()
+            $('#tiendas_proyectos_descripcion').prepend(`<p class="proyectos__text" id="tiendas_proyectos_cliente">${cliente}</p> ` + descripcion)
+        }).fadeTo(500,1);
+    
+        $("#tiendas_proyectos_img").fadeTo(500,0.10, function() {
+            $('#tiendas_proyectos_img').attr("src", img);
+        }).fadeTo(500,1);
+        return false;
 
     }
-
-
-    $('#tiendas_proyectos_descripcion').fadeOut(500, function() {
-        //$('#tiendas_proyectos_descripcion').text(descripcion).fadeIn(500);
-    }).fadeIn(500);
-
-    $('#tiendas_proyectos_cliente').fadeTo(500,0.10, function() {
-        $('#tiendas_proyectos_descripcion').empty().show()
-        $('#tiendas_proyectos_descripcion').prepend(`<p class="proyectos__text" id="tiendas_proyectos_cliente">${cliente}</p> ` + descripcion)
-    }).fadeTo(500,1);
-
-    $("#tiendas_proyectos_img").fadeTo(500,0.10, function() {
-        $('#tiendas_proyectos_img').attr("src", img);
-    }).fadeTo(500,1);
-    return false;
-
 
 })
