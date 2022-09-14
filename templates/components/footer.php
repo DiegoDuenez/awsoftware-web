1659f531b1f8392ebfecea616bb066e13c5961db
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-09-14 15:17:31 -0500

    change: Change footer

--- templates/components/footer.php
+++ templates/components/footer.php
@@ -4,10 +4,10 @@
         <a class="footer__text">A W SOFTWARE 2012 - <?php echo date("Y");?><a href="aviso.pdf" target="blank" class="footer__text footer__text--azul">AVISO DE PRIVACIDAD</a></a>
     </div>
 
-    <div class="footer__content" >
+    <div class="footer__content" id="tag">
         <a href="https://es-la.facebook.com/awsoftware" target="blank"><i class="fa-brands fa-facebook-square footer__icon"></i></a>
         <a href="https://www.instagram.com/awsoftware/?hl=en" target="blank"><i class="fa-brands fa-instagram footer__icon"></i></a>
-        <p class="footer__text">
+        <p class="footer__text" >
             #10AÑOSSIMPLIFICANDOVIDAS
         </p>
     </div>
